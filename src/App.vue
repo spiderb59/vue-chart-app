7638d4624869087875d98853e8cf0fa6e00c43a8
Shital Mainali <36978486+spiderb59@users.noreply.github.com>
2024-04-25 11:01:49 +0900

    Importing dependencies

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,9 @@
     <canvas id="myChart"></canvas>
   </div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import Chart from 'chart.js/auto';
+import jsonData from './data.json'; 
+</script>
